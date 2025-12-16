a63e72235be217bb7e6643df5d3371f88aad09d0
Mits <hoanglonglpgxxx@gmail.com>
2025-12-16 17:30:24 +0700

    test

--- platforms/steam.js
+++ platforms/steam.js
@@ -1,9 +1,12 @@
 const SteamUser = require('steam-user');
+const fs = require('fs');
 const { debugLog } = require('../utils/helper');
 const { taskAuto } = require('../actions/cronJobs');
 
-const CHECK_INTERVAL = 12 * 60 * 60 * 1000;
-const APP_ID = 247060;
+require('dotenv').config();
+
+const STATE_FILE = './last_change.json';
+const APP_IDS = [247060, 570];
 
 let steamGuardCallback = null;
 const STEAM_ACC = {
@@ -12,57 +15,34 @@ const STEAM_ACC = {
 };
 const steamClient = new SteamUser();
 
-function steamHandler(lastChangeNumber) {
-    steamClient.setOption('promptSteamGuardCode', false);
-    steamClient.logOn(STEAM_ACC);
-
-    steamClient.on('loggedOn', async () => {
-        steamGuardCallback = null;
-
-        debugLog(`[STEAM] Đang request license cho App ${APP_ID}...`);
-        steamClient.requestFreeLicense([APP_ID], (err, grantedPackages, grantedAppIds) => {
-            setTimeout(() => {
-                debugLog('[STEAM] 🚀 Bắt đầu theo dõi Changelist...');
-                autoCheckUpdate();
-                taskAuto('0 0,12 * * *', autoCheckUpdate);
-            }, 5000);
-        });
-
-    });
-
-    steamClient.on('steamGuard', (domain, callback) => {
-        debugLog('[!!!] STEAM YÊU CẦU MÃ CODE. Vui lòng chat trên Discord: !code <mã_số>');
-        steamGuardCallback = callback;
-    });
-
-    steamClient.on('error', (err) => console.log(new Date().toLocaleString('vi-VN', {}), '[STEAM ERROR]', err));
-}
-
-function getSteamUpdateInfo() {
+function getSteamUpdateInfo(appId) {
     return new Promise((resolve, reject) => {
-        steamClient.getProductInfo([APP_ID], [], true, (err, apps) => {
-            if (err) return reject(new Error(`Lỗi kết nối Steam: ${err.message}`));
+        steamClient.getProductInfo([appId], [], (err, apps, packages) => {
+            if (err) return reject(err);
 
-            const appData = apps[APP_ID];
-            if (!appData) return reject(new Error("Không tìm thấy dữ liệu App (Đang chờ License hoặc sai ID)"));
+            const appData = apps[appId];
+            if (!appData) return reject(new Error(`No data for AppID: ${appId}`));
 
             let changeNum = appData.changenumber;
             if (!changeNum && appData.appinfo) {
                 changeNum = appData.appinfo.changenumber;
             }
 
-            if (!changeNum) return reject(new Error("Dữ liệu về (OK) nhưng không có Change Number."));
+            if (!changeNum) return reject(new Error(`(OK) but no Change Number for ${appId}.`));
 
             let finalName = "";
-            if (APP_ID === 247060) {
+            if (appId === 247060) {
                 finalName = "SteamDB Unknown App 247060 (Dota 2 Test 2 - Dedicated Server)";
+            } else if (appId === 570) {
+                finalName = "Dota 2";
             } else {
                 finalName = (appData.appinfo && appData.appinfo.common && appData.appinfo.common.name)
                     ? appData.appinfo.common.name
-                    : `Unknown App ${APP_ID}`;
+                    : `Unknown App ${appId}`;
             }
 
             resolve({
+                appId: appId,
                 changeNumber: changeNum,
                 name: finalName
             });
@@ -70,25 +50,66 @@ function getSteamUpdateInfo() {
     });
 }
 
-async function autoCheckUpdate(lastChangeNumber) {
+async function autoCheckUpdate(lastChangeState) {
     try {
         if (!steamClient.steamID) return;
-        const info = await getSteamUpdateInfo();
 
-        if (info.changeNumber > lastChangeNumber) {
-            debugLog(`[UPDATE] Detect new Changelist: ${info.changeNumber}`);
-            lastChangeNumber = info.changeNumber;
-            fs.writeFileSync(STATE_FILE, JSON.stringify({ changeNumber: lastChangeNumber }));
-        } else {
-            debugLog(`[UPDATE] Nothing new`);
+        let hasUpdates = false;
+
+        for (const appId of APP_IDS) {
+            try {
+                const info = await getSteamUpdateInfo(appId);
+                const currentLastChange = lastChangeState[appId] || 0;
+
+                if (info.changeNumber > currentLastChange) {
+                    debugLog(`[UPDATE] Detect new Changelist for ${info.name} (${appId}): ${info.changeNumber}`);
+
+                    lastChangeState[appId] = info.changeNumber;
+                    hasUpdates = true;
+                } else {
+                    debugLog(`[UPDATE] Nothing new for ${appId}`);
+                }
+            } catch (innerError) {
+                console.error(`Error checking App ${appId}:`, innerError.message);
+            }
         }
-    } catch (e) {
-        console.error('[AUTO CHECK ERROR]', e.message);
+
+        if (hasUpdates) {
+            fs.writeFileSync(STATE_FILE, JSON.stringify(lastChangeState, null, 2));
+        }
+
+    } catch (err) {
+        console.error('Error in autoCheckUpdate:', err);
     }
 }
 
-module.exports = {
-    steamClient,
-    getSteamUpdateInfo,
-    steamHandler
-};
+function steamHandler(lastChangeState) {
+    steamClient.setOption('promptSteamGuardCode', false);
+    steamClient.logOn(STEAM_ACC);
+
+    steamClient.on('loggedOn', async () => {
+        steamGuardCallback = null;
+
+        debugLog(`[STEAM] Đang request license cho Apps ${APP_IDS.join(', ')}...`);
+
+        steamClient.requestFreeLicense(APP_IDS, (err, grantedPackages, grantedAppIds) => {
+            setTimeout(() => {
+                debugLog('[STEAM] 🚀 Bắt đầu theo dõi Changelist...');
+
+                autoCheckUpdate(lastChangeState);
+
+                taskAuto('0 0,12 * * *', () => autoCheckUpdate(lastChangeState));
+            }, 5000);
+        });
+    });
+
+    steamClient.on('steamGuard', (domain, callback) => {
+        debugLog('[!!!] STEAM YÊU CẦU MÃ CODE. Vui lòng chat trên Discord: !code <mã_số>');
+    });
+
+    steamClient.on('error', (err) => {
+        console.error('Steam login error:', err);
+    });
+}
+
+module.exports = { steamHandler, getSteamUpdateInfo, steamClient };
