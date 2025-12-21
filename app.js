900dc19fd5d5a8832b8228f489f15ae3e1b6df74
Mít <hoanglonglpgxxx@gmail.com>
2025-12-21 17:30:32 +0700

    fix

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/api/v1/reminder', reminderRouter); //method : Mounting Router
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
