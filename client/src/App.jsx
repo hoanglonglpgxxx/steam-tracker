63c1c6dbfba6767639a7c36bb3354cbfb7c6b9d4
Mít <hoanglonglpgxxx@gmail.com>
2025-12-21 17:51:15 +0700

    up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
 // client/src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UpdateTime from "./UpdateTime"; // Import file vừa tạo
+import UpdateTime from "./UpdateTime";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Đây là nơi định nghĩa đường dẫn */}
-        {/* Khi người dùng vào link /reminder/123/update -> Hiện giao diện UpdateTime */}
         <Route path="/reminder/:id/update" element={<UpdateTime />} />
-
-        {/* Bạn có thể thêm các route khác sau này */}
       </Routes>
     </BrowserRouter>
   );
